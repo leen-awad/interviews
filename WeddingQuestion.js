369f5227e33af2951904a1485780b9e86a08e48a
leen awad <kleen.awad@gmail.com>
2021-09-17 16:15:51 +0300

    Add files via upload

---
+++ WeddingQuestion.js
@@ -0,0 +1,9 @@
+for (var x=0; x<= 200; x++){ 
+    for (var y=0; y<= 200; y++){ 
+        for (var z=0; z<= 200; z++){ 
+          if((x+y+z ==200) && (x*0.2+y*2+z*10 == 200)){
+          console.log(x +" Drinks, "+y+" Dounts, "+z+ " Burgers")
+        }
+    }
+  }
+}
